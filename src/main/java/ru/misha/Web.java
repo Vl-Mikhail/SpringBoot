4e1d4b540af9ff8c623748a22bca0f5abfd57a98
Vl-Mikhail <evil_bear@mail.ru>
2016-08-17 23:03:15 +1000

    Initial commit

---
+++ src/main/java/ru/misha/Web.java
@@ -0,0 +1,13 @@
+package ru.misha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Web {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello-world";
+    }
+}
