54f1234a6570b296eab94ca96398801b1ce21871
Михаил <evil_bear@mail.ru>
2016-08-20 17:01:58 +1000

    Добавил AngularJS

--- angular/js/main.js
+++ angular/js/main.js
@@ -2,21 +2,52 @@
 var app = angular.module("angularDemo", []);
 
 //Добавление нового фильтра
-app.filter('reverse', function () {
-    return function (text) {
-        return text.split("").reverse().join("");
-    }
+//app.filter('reverse', function () {
+//    return function (text) {
+//        return text.split("").reverse().join("");
+//    }
+//});
+
+//app.controller("MyController", function ($scope) {
+//    $scope.data = {
+//        message: ""
+//    };
+    //Метод возращает перевернутое значение
+//    $scope.reversedMessage = function (message) {
+//        return message.split("").reverse().join("");
+//    }
+//
+//});
+
+app.controller("MyController", function ($scope, filmService) {
+    $scope.films =  filmService.getFilms();
 });
 
-app.controller("MyController", function ($scope) {
-
-    $scope.data = {
-        message: ""
+app.factory('filmService', function(){
+    return {
+        getFilms: function(){
+            return [
+                {
+                    "id": "1",
+                    "name" : "Russian"
+                },
+                {
+                    "id": "1",
+                    "name" : "29"
+                },
+                {
+                    "id": "1",
+                    "name" : "JP"
+                },
+                {
+                    "id": "1",
+                    "name" : "29"
+                },
+                {
+                    "id": "1",
+                    "name" : "29"
+                }
+            ];
+        }
     };
-
-    //Метод возращает перевернутое значение
-    $scope.reversedMessage = function (message) {
-        return message.split("").reverse().join("");
-    }
-
 });
